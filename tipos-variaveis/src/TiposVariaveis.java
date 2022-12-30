311c4fc5936994d30e913fff4a0ccb37f30000b5
Elisberto Junior <elisberto_jr@hotmail.com>
2022-12-29 22:20:43 -0300

    add new project

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,3 +1,6 @@
+import java.net.SocketPermission;
+import java.sql.Date;
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500;
@@ -5,5 +8,19 @@ public class TiposVariaveis {
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal; //casting
+
+        //constante
+        final int ANO = 2021;
+
+        //classe operadores.java
+
+        String nome = "Junior";
+        int idade = 29;
+        double peso = 92.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date(idade);
+
+        
     }
 }
