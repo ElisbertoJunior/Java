8c1b8be5a501035b48903d64043296ec78846855
Elisberto Junior <elisberto_jr@hotmail.com>
2022-12-30 15:01:57 -0300

    smartTv

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,32 @@
+
+public class SmartTv {
+  boolean ligada = false;
+  int canal = 1;
+  int volume = 25;
+
+  public void ligar() {
+    ligada = !ligada;
+  }
+
+  public void aumentarVolume() {
+    volume++;
+  }
+
+  public void abaixarVolume() {
+    volume--;
+  }
+
+  public void maisCanal() {
+    canal++;
+  }
+
+  public void menosCanal() {
+    canal--;
+  }
+
+  public void mudarCanal(int novoCanal) {
+    canal = novoCanal;
+  }
+  
+
+}
---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,21 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada ? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("TV ligada ? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        System.out.println("volume atual ? " + smartTv.volume);
+
+        smartTv.mudarCanal(11);
+        System.out.println("Canal atual: " + smartTv.canal);
+    }
+}
