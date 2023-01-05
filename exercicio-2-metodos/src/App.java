380420751a8393daa46992297debb5e77f01d2c6
Elisberto Junior <elisberto_jr@hotmail.com>
2023-01-05 20:43:26 -0300

    adicionando exercicio 2

---
+++ exercicio-2-metodos/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+
+    }
+}
