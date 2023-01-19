c2d3f00d9a4e5bb28b40e830bd2f9b043ce1ec62
Elisberto Junior <elisberto_jr@hotmail.com>
2023-01-18 21:04:35 -0300

    finalizando

--- exercicio-2-metodos/src/App.java
+++ exercicio-2-metodos/src/App.java
@@ -1,11 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       
+       /* 
         System.out.println("Exercício quadrilátero");
         Quadrilatero.area(3);
         Quadrilatero.area(5d,5d);
         Quadrilatero.area(7,8,9);
         Quadrilatero.area(5f,5f);
+    */
+    int entrada = 2;
+    int mesada = 50;
+
+    int total = entrada * mesada;
+        
+    System.out.println("Voce tera " + total + " reais");
 
     }
 }
