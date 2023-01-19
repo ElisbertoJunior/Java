dfeac15ceeeeee2aa0ee30f650df051004fa0371
Elisberto Junior <elisberto_jr@hotmail.com>
2023-01-19 20:34:13 -0300

    inicando exercicio

---
+++ orientacao-objetos/exercicio-01/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String name = "Jetta";
+
+       Car.carName(name);
+    }
+}
+
+
---
+++ orientacao-objetos/exercicio-01/src/Car.java
@@ -0,0 +1,7 @@
+public class Car {
+
+  public static void carName(String name) {
+    System.out.println("O nome do carro e " + name);
+  }
+  
+}
