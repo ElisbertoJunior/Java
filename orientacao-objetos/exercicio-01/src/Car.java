c53cf0690aace41e7d5cd272cda537d16b0062b2
Elisberto Junior <elisberto_jr@hotmail.com>
2023-01-19 21:16:45 -0300

    inicando exercicio

--- orientacao-objetos/exercicio-01/src/Car.java
+++ orientacao-objetos/exercicio-01/src/Car.java
@@ -1,10 +1,16 @@
 public class Car {
   static String name = "Gol";
   static String color = "Prata";
-  static  int tankCapacity = 45;
+  static int tankCapacity = 45;
     
   public static void carInfo() {
     System.out.println("O nome do carro e " + name + " a cor e " + color + " e tem capacidade para " + tankCapacity + " litros de combustivel.");
+  }
+
+  public static double valueToFillTheTank(double literValue) {
+ 
+    return (double) (literValue * tankCapacity);
+    
   }
   
 }
