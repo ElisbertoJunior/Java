8fab66979bce9d1b4fe9606b61606794029ca3c9
Elisberto Junior <elisberto_jr@hotmail.com>
2023-01-20 20:30:04 -0300

    finalizando

--- orientacao-objetos/exercicio-2/src/App.java
+++ orientacao-objetos/exercicio-2/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
+        Funcionario funcionario = new Funcionario();
+
+        //up cast
+        Funcionario gerente = new Gerente();
+        Funcionario vendedor = new Vendedor();
+        Funcionario faxineiro = new Funcionario();
+
+
+        //down cast
+        Vendedor vendedor_ = (Vendedor) new Funcionario();
+
     }
 }
