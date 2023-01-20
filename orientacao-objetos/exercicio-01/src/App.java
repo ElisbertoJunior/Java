62452b1b11ce4ddc26ef3728c3668fd9f50854b9
Elisberto Junior <elisberto_jr@hotmail.com>
2023-01-19 22:03:46 -0300

    finalizando exercicio

--- orientacao-objetos/exercicio-01/src/App.java
+++ orientacao-objetos/exercicio-01/src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
       Car carro1 = new Car();
 
        carro1.setModel("Gol");         
@@ -12,7 +12,19 @@ public class App {
 
        System.out.println("Valor para encher o tanque e " + carro1.valueToFillTheTank(5.37));
 
+
+       //exemplo com propriedades passadas por parametro
+
+       Car carro2 = new Car("jetta", "Prata", 60);
+
+       System.out.println(carro2.getModel());
+       System.out.println(carro2.getColor());
+       System.out.println(carro2.getTankCapacity());
+
+       System.out.println("Valor para encher o tanque e " + carro2.valueToFillTheTank(5.37));
+
     }
+    
 }
 
 
