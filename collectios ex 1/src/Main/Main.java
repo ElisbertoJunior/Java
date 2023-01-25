1cd383c85984fdd19f35ac7d19ec34b07b106b4a
Elisberto Junior <elisberto_jr@hotmail.com>
2023-01-25 12:12:14 -0300

    iniciando collections

---
+++ collectios ex 1/src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Double> notas = new ArrayList<>();
+    }
+}
