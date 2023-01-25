58fae318f07a68fafa9fd89a1e3e408613086082
Elisberto Junior <elisberto_jr@hotmail.com>
2023-01-25 12:34:58 -0300

    iniciando collections

---
+++ collectios ex 1/src/Main/ComoIniciarList/Main.java
@@ -0,0 +1,32 @@
+package Main.ComoIniciarList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+      /*
+            List<Double> notas = new ArrayList<>();
+            ArrayList<Double> notas2 = new ArrayList<>();
+
+            notas.add(1d);
+
+            //Exemplo ja adicionando valores
+            List<Double> notas3 = new ArrayList<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
+            List<Double> notas4 = Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
+
+            //Lista imutavel na lista imutael uasamos "Lista.of"
+            List<Double> notas5 = List.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
+            //notas5.add(5d); operacao nao sera reizada pois notas5 e imutavel
+
+            System.out.println(notas3);
+       */
+
+        System.out.println("Crie um alista e adicione 7 notas.");
+
+        List<Double> notas = new ArrayList<>();
+
+    }
+}
