c6638814c80112e41647e7dcb4a4d906ad06a495
Elisberto Junior <elisberto_jr@hotmail.com>
2023-02-03 23:44:05 -0300

    iniciando

---
+++ sistema-oficina-acelerados/sistema-acelerados/src/br/com/acelerados/telas/TelaLogin.java
@@ -0,0 +1,13 @@
+package br.com.acelerados.telas;
+
+import javax.swing.*;
+
+public class TelaLogin {
+    public static void main(String[] args) {
+        new TelaLogin();
+    }
+    public TelaLogin() {
+        JFrame jFrame = new JFrame();
+        JFrame.setVisible(true);
+    }
+}
